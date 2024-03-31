21e9486e7e823041e34c996c7a8dd008d026797c
Hassan <hasangh19@yahoo.com>
2024-03-31 18:03:13 +0200

    main1

---
+++ main.py
@@ -0,0 +1,12 @@
+from add import my_add
+from multiply import my_multiply
+
+a = 2
+b = 3
+
+c = my_add(a,b)
+d = my_multiply(a,b)
+print(f"add: {c}")
+print(f"multiply: {d}")
+
+print('somthing is added to main')
